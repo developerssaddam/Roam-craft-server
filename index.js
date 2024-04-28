6581ca657b2fcdbd86606ab0c00d1719569ec18b
developersaddam <developerssaddam@gmail.com>
2024-04-28 20:43:52 +0600

    Added Delete route

--- index.js
+++ index.js
@@ -66,6 +66,14 @@ async function run() {
       const result = await touristspotCollection.insertOne(data);
       res.send(result);
     });
+
+    // DeleteTourist spot
+    app.delete("/touristspot/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristspotCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
